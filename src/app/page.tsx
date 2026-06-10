b6fbf8f182917a64d04cb0e30f1bc7d29ea0080a
smile189 <email-ul-tau-de-github@exemplu.com>
2026-06-10 14:50:00 +0300

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Hero from "@/components/Hero";
 import Transformare3D from "@/components/Transformare3D";
 import Footer from "@/components/Footer";
 import Manifestdesign from "@/components/Manifestdesign";
-import ChatBot from "@/components/ChatBot";
+
 
 
 export default function Home() {

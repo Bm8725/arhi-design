f345a332d3efd1122ed583e9562b2cdf8bd73266
smile189 <email-ul-tau-de-github@exemplu.com>
2026-08-22 10:08:11 +0300

    webm hero prev

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -45,6 +45,7 @@ export default function Hero() {
           muted
           loop
           playsInline
+          poster="/nimet.webp"
           src="/arhidesign.webm"
           className="absolute inset-0 w-full h-full object-cover"
         />
